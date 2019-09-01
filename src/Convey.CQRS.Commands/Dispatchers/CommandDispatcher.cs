83844c394c834cdecc66f3ebd2bf03263d953e0a
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-09-01 16:37:19 +0200

    Handler

--- src/Convey.CQRS.Commands/Dispatchers/CommandDispatcher.cs
+++ src/Convey.CQRS.Commands/Dispatchers/CommandDispatcher.cs
@@ -12,17 +12,13 @@ namespace Convey.CQRS.Commands.Dispatchers
         {
             _serviceFactory = serviceFactory;
         }
-        
+
         public Task SendAsync<T>(T command) where T : class, ICommand
         {
             using (var scope = _serviceFactory.CreateScope())
             {
-                var handler = scope.ServiceProvider.GetService<ICommandHandler<T>>();
-                if (handler is null)
-                {
-                    throw new InvalidOperationException($"Command handler for: '{command}' was not found.");
-                }
-                
+                var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<T>>();
+
                 return handler.HandleAsync(command);
             }
         }
